8bbaa3c5b9288f01e24243b1003e7e234702a44d
choieuntaek <choiet0225@gmail.com>
2023-11-27 21:14:37 +0900

    settings

--- ThingiMarket/src/main/java/com/example/thingimarket/repository/post/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.thingimarket.repository.post;
-
-import com.example.thingimarket.domain.post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- ThingiMarket/src/main/java/com/example/thingimarket/repository/transaction/TransactionPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.thingimarket.repository.transaction;
-
-import com.example.thingimarket.domain.transaction.TransactionPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionPostRepository extends JpaRepository<TransactionPost, Long> {
-}
