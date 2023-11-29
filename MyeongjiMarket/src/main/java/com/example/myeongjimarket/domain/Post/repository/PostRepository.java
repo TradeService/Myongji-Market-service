44c33857403a0e23cc1df17733222831c6452c9f
choieuntaek <choiet0225@gmail.com>
2023-11-29 09:54:42 +0900

    Remove all files

--- MyeongjiMarket/src/main/java/com/example/myeongjimarket/domain/Post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myeongjimarket.domain.Post.repository;
-
-import com.example.myeongjimarket.domain.Post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
