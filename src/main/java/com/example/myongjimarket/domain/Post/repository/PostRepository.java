2cb22821447ba4ac5dd43b8c847e63e143ef5ef1
eeeseohyun <eeeseohyun@gmail.com>
2023-12-18 21:09:38 +0900

    공동구매 기능

--- src/main/java/com/example/myongjimarket/domain/Post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myongjimarket.domain.Post.repository;
-
-import com.example.myongjimarket.domain.Post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
